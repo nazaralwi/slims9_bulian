9e43d6805a8f402a78bff0972df200d639d6c3fd
Nazar Alwi <alwinazar75@gmail.com>
2023-12-24 21:06:46 +0700

    Show reservation document

--- plugins/reservasi_ruang_diskusi/app/admin/reserved_schedule_grid.inc.php
+++ plugins/reservasi_ruang_diskusi/app/admin/reserved_schedule_grid.inc.php
@@ -1,5 +1,7 @@
 <?php
 
+use SLiMS\Filesystems\Storage;
+
 // create datagrid
 $datagrid = new simbio_datagrid();
 
@@ -10,7 +12,7 @@ $table_spec = 'room_reservations';
 $datagrid->setSQLColumn('reservation_id AS \''.__('ID Reservasi').'\'',
                         'reservation_date AS \''.__('Tanggal Reservasi').'\'', 
                         'name AS \''.__('Nama').'\'', 
-                        'reservation_document_id AS \''.__('ID Dokumen').'\'', 
+                        'reservation_document_id AS \''.__('Dokumen').'\'', 
                         'student_id AS \''.__('NIM').'\'', 
                         'major AS \''.__('Jurusan').'\'',
                         'whatsapp_number AS \''.__('Nomor WhatsApp').'\'',
@@ -24,10 +26,27 @@ $datagrid->setSQLColumn('reservation_id AS \''.__('ID Reservasi').'\'',
 // ordering
 $datagrid->setSQLorder('reservation_date DESC');
 // $datagrid->setSQLCriteria('reserved_date = CURRENT_DATE AND ((reserved_date = CURRENT_DATE AND end_time > CURRENT_TIME) OR reserved_date > CURRENT_DATE)');
-$datagrid->modifyColumnContent(6, 'callback{createLinkableWhatsAppNumber}');
+$datagrid->modifyColumnContent(3, 'callback{createLinkableReservationDocument}'); // modify reservation_document_id content
+$datagrid->modifyColumnContent(6, 'callback{createLinkableWhatsAppNumber}'); // modify whatsapp_number content
+
+function createLinkableReservationDocument($obj_db, $row, $field_num) {
+    $documentId = $row[$field_num];
+    if ($documentId != 0) {
+        $query = 'SELECT files.file_title
+        FROM room_reservations
+        JOIN files ON room_reservations.reservation_document_id = files.file_id
+        WHERE room_reservations.reservation_document_id = ' . $row[$field_num];
+        $result = $obj_db->query($query);
+
+        $filesRow = $result->fetch_assoc();
+        return '<a class="s-btn btn btn-link notAJAX openPopUp" href="'.SWB.'admin/view.php?fid='.urlencode($row[$field_num]).'" width="780" height="500" target="_blank">'.$filesRow['file_title'].'</a>';;
+    } else {
+        return '<a>Tidak ada surat</a>';
+    }
+}
 
 function createLinkableWhatsAppNumber($obj_db, $row, $field_num) {
-    return '<a href="https://wa.me/' . $row[$field_num] .'" target="_blank" style="color: blue; text-decoration: underline;">'. $row[$field_num] .'</a>';
+    return '<a href="https://wa.me/' . $row[$field_num] .'" target="_blank" style="color: blue; text-decoration: underline;">'.$row[$field_num].'</a>';
 }
 
 // set table and table header attributes
